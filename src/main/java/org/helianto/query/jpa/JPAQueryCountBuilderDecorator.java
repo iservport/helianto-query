6eee0cf930485a17e246e117ea7b2b6e753dd079
Mauricio Fernandes de Castro <mauricio@iservport.com>
2014-04-02 10:48:48 -0300

    Inclui decorador para permitir operações de contagem.

---
+++ src/main/java/org/helianto/query/jpa/JPAQueryCountBuilderDecorator.java
@@ -0,0 +1,92 @@
+package org.helianto.query.jpa;
+
+import org.helianto.query.QueryBuilder;
+
+/**
+ * Decora um {@link AbstractQueryBuilder} para modificar a cláusula de seleção e permitir contagem.
+ * 
+ * @author mauriciofernandesdecastro
+ */
+public class JPAQueryCountBuilderDecorator 
+	extends JPAQueryBuilder
+{
+	private final AbstractQueryBuilder query;
+	
+	public JPAQueryCountBuilderDecorator(AbstractQueryBuilder query) {
+		super(query.getClazz());
+		this.query = query;
+	}
+	
+	@Override
+	protected StringBuilder getInternalBuilder() {
+		return query.getInternalBuilder();
+	}
+
+	@Override
+	protected StringBuilder select() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("select count(").append(getAlias()).append(".id) ");
+		return builder;
+	}
+	
+	@Override
+	protected StringBuilder from() {
+		return query.from();
+	}
+	
+	@Override
+	protected StringBuilder orderBy() {
+		return query.orderBy();
+	}
+
+	@Override
+	public String getAlias() {
+		return query.getAlias();
+	}
+
+	@Override
+	public QueryBuilder append(String token) {
+		return query.append(token);
+	}
+
+	@Override
+	public QueryBuilder eq(String fieldName, String constraint) {
+		return query.eq(fieldName, constraint);
+	}
+
+	@Override
+	public QueryBuilder eq(String fieldName, int constraint) {
+		return query.eq(fieldName, constraint);
+	}
+
+	@Override
+	public QueryBuilder eq(String fieldName, long constraint) {
+		return query.eq(fieldName, constraint);
+	}
+
+	@Override
+	public QueryBuilder eq(String fieldName, char constraint) {
+		return query.eq(fieldName, constraint);
+	}
+
+	@Override
+	public QueryBuilder like(String fieldName, String constraint) {
+		return query.like(fieldName, constraint);
+	}
+
+	@Override
+	public QueryBuilder and() {
+		return query.and();
+	}
+
+	@Override
+	public QueryBuilder or() {
+		return query.or();
+	}
+
+	@Override
+	public QueryBuilder not() {
+		return query.not();
+	}
+
+}
---
+++ src/test/java/org/helianto/query/jpa/JPAQueryCountBuilderDecoratorTests.java
@@ -0,0 +1,24 @@
+package org.helianto.query.jpa;
+
+import static org.junit.Assert.assertEquals;
+
+import org.helianto.query.QueryBuilder;
+import org.helianto.query.TestingDomainObject;
+import org.junit.Test;
+
+/**
+ * 
+ * @author mauriciofernandesdecastro
+ */
+public class JPAQueryCountBuilderDecoratorTests {
+
+	@Test
+	public void test() {
+		JPAQueryBuilder query = new JPAQueryBuilder(TestingDomainObject.class);
+		QueryBuilder decoratedQuery = new JPAQueryCountBuilderDecorator(query);
+		query.eq("FIELD", "1");
+		assertEquals("select count(alias.id) from TestingDomainObject where alias.FIELD = '1'"
+				, decoratedQuery.build());
+	}
+
+}
